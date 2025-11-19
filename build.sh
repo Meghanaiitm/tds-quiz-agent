b7ce8c3de7fe1b4ac0874b2eb80d33ad55e01c91
Meghanaiitm <23f2001531@ds.study.iitm.ac.in>
2025-11-19 08:27:54 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,11 @@
-#!/bin/bash
+#!/usr/bin/env bash
+set -o errexit
+
+# Install Python packages
 pip install -r requirements.txt
-python -m playwright install
+
+# Install Playwright browsers (important!)
+python -m playwright install chromium
+
+# (Optional) Install dependencies for webkit and firefox
+# python -m playwright install firefox webkit
